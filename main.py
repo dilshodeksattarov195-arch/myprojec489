e794e0826c7cc3ec6da74e300c36965a8111325c
DILSHODBEK <dilshodsattar26@gmail.com>
2026-05-30 06:33:30 +0500

    Initial commit: setup main.py

---
+++ main.py
@@ -0,0 +1,13 @@
+import sys
+import json
+
+class helperPncryptWorker:
+    def __init__(self):
+        self.buffer = []
+    def append_metric(self, val):
+        self.buffer.append(val)
+        return len(self.buffer)
+
+if __name__ == '__main__':
+    obj = helperPncryptWorker()
+    print("Worker engine initialized.")
